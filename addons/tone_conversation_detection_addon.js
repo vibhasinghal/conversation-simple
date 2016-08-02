c5b3c0f074213e5046d7f8a1f1909761f3d8bba0
April Webster <awebster@us.ibm.com>
2016-08-02 15:07:46 -0700

    documented functions

--- addons/tone_conversation_detection_addon.js
+++ addons/tone_conversation_detection_addon.js
@@ -1,6 +1,10 @@
 require('dotenv').config({silent: true});
 var watson = require('watson-developer-cloud');  // watson sdk
 
+/**
+ * Instantiate the Watson Tone Analyzer
+ * Credentials must be provided and can be found on the Bluemix Platform
+ */
 var tone_analyzer = watson.tone_analyzer({
 	url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
 	username: process.env.TONE_ANALYZER_USERNAME,
@@ -9,20 +13,37 @@ var tone_analyzer = watson.tone_analyzer({
 	version: 'v3'
 });
 
-
-var toneAnalyzer = null;
+/**
+ * Thresholds for identifying meaningful tones returned by the Watson Tone Analyzer.
+ * For more information: https://www.ibm.com/watson/developercloud/doc/tone-analyzer/understanding-tone.shtml
+ */ 
 var EMOTION_SCORE_THRESHOLD = 0.5;
 var LANGUAGE_SCORE_THRESHOLD = 0.6;
 var SOCIAL_SCORE_THRESHOLD = 0.75;
 
+/**
+ * Labels for the different tones returned by the Watson Tone Analyzer
+ */
+var EMOTION_TONE_LABEL = "emotion_tone";
+var LANGUAGE_TONE_LABEL = "language_tone";
+var SOCIAL_TONE_LABEL = "social_tone";
 
+
+/**
+ * Public functions for this module
+ */
 module.exports = {
   initToneContext,
   invokeTone,
   updateUserTone
 };
 
-
+/**
+ * initToneContext initializes a user object containing tone data (from the Watson Tone Analyzer)
+ * @returns user json object with the emotion, language and social tones.  The current
+ * tone identifies the tone for a specific conversation turn, and the history provides the conversation for 
+ * all tones up to the current tone for a conversation instance with a user.
+ */
 function initToneContext() {
     return {
       'user': {
@@ -44,6 +65,13 @@ function initToneContext() {
     };
 };
 
+/**
+ * invokeTone calls the Watson Tone Analyzer with the (input) text and passes the payload returned by the 
+ * Tone Analyzer to the callback function.
+ * @param text - the input text provided by a user during a conversation turn
+ * @param callback - the callback function to invoke after the Watson Tone Analyzer has processed the (input) text and returned a payload
+ * @returns none
+ */
 function invokeTone(text, callback) {
 	var toneAnalyzerPayload = {
       text: text
@@ -60,7 +88,14 @@ function invokeTone(text, callback) {
   };
 
 
-  
+/**
+ * updateUserTone processes the Tone Analyzer payload, if not erroneous, to pull out the emotion, language and social
+ * tones, and further processes this data to identify the meaningful tones (i.e., those tones that meet a 
+ * specified threshold).  The user json object is updated to includes these tones.
+ * @param user
+ * @param toneAnalyzerPayload
+ * @returns
+ */
 function updateUserTone (user, toneAnalyzerPayload) {
     var emotionTone = null;
     var languageTone = null;
@@ -71,13 +106,13 @@ function updateUserTone (user, toneAnalyzerPayload) {
 
     if (toneAnalyzerPayload && toneAnalyzerPayload.document_tone) {
       toneAnalyzerPayload.document_tone.tone_categories.forEach(function(toneCategory) {
-        if (toneCategory.category_id === "emotion_tone") {
+        if (toneCategory.category_id === EMOTION_TONE_LABEL) {
           emotionTone = toneCategory;
         }
-        if (toneCategory.category_id === "language_tone") {
+        if (toneCategory.category_id === LANGUAGE_TONE_LABEL) {
           languageTone = toneCategory;
         }
-        if (toneCategory.category_id === "social_tone") {
+        if (toneCategory.category_id === SOCIAL_TONE_LABEL) {
           socialTone = toneCategory;
         }
       });
@@ -109,6 +144,12 @@ function updateUserTone (user, toneAnalyzerPayload) {
     return user;
 };  
   
+/**
+ * getEmotionProfile identifies the primary emotion in the emotion tones in the payload returned by the Tone Analyzer
+ * @param emotionTone a json object containing the emotion tones in the payload returned by the Tone Analyzer
+ * @returns the primary emotion if one of the emotion tones has a score that is greater than or equal to the 
+ * EMOTION_SCORE_THRESHOLD; otherwise, returns 'neutral'
+ */
 function getEmotionProfile(emotionTone) {
   var maxScore = 0;
   var primaryEmotion = null;
@@ -128,6 +169,12 @@ function getEmotionProfile(emotionTone) {
   return primaryEmotion;
 }
 
+/**
+ * getLanguageProfile identifies the language tones that are greater than the LANGUAGE_SCORE_THRESHOLD
+ * @param languageTone a json object containing the language tones in the payload returned by the Tone Analyzer
+ * @returns a space-separated string containing the language tones that have a score that meets or exceeds the
+ * LANGUAGE_SCORE_THRESHOLD
+ */
 function getLanguageProfile(languageTone) {
   var languageProfile = '';
 
@@ -140,6 +187,12 @@ function getLanguageProfile(languageTone) {
   return languageProfile;
 };
 
+/**
+ * getSocialProfile 
+ * @param socialTone a json object containing the social tones in the payload returned by the Tone Analyzer
+ * @returns a space-separated string containing the social tones that have a score that meets or exceeds the
+ * SOCIAL_SCORE_THRESHOLD
+ */
 function getSocialProfile(socialTone) {
   var socialProfile = '';
 
